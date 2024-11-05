a12aa2ac307c4e5932a4afac4b497cb21bfb77ef
savvas-paragkamian <s.paragkamian@gmail.com>
2024-11-05 13:46:19 +0200

    figure for family analysis

--- scripts/araneae_gr_analysis.R
+++ scripts/araneae_gr_analysis.R
@@ -64,7 +64,7 @@ araneae_gr_occ_sf <- araneae_gr_occ |>
 
 araneae_gr_inside_gr <- st_intersection(araneae_gr_occ_sf, hellenic_borders_shp)
 
-## figures
+## map
 
 araneae_gr_base <- ggplot() +
     geom_sf(hellenic_borders_shp, mapping=aes()) +
@@ -89,3 +89,61 @@ ggsave("../figures/araneae_gr_base.png",
        dpi = 600, 
        units="cm",
        device="png")
+
+## taxonomic summary
+
+araneae_gr_occ_tax <- araneae_gr_occ_sf |>
+    left_join(araneae_gr_tax)
+
+araneae_gr_occ_tax_f <- araneae_gr_occ_tax |> 
+    st_drop_geometry() |>
+    group_by(Species,Family) |>
+    summarise(occurrences=n(), .groups="keep") |> 
+    group_by(Family) |>
+    summarise(n_species=n(),occurrences=sum(occurrences))
+
+araneae_gr_occ_tax_f_l <- araneae_gr_occ_tax_f |>
+    pivot_longer(-Family, names_to="Variable", values_to="number")
+
+species_family_plot <- ggplot()+
+  geom_col(data = araneae_gr_occ_tax_f_l,
+           aes(x=Family, y= number, fill=Variable),
+           width=0.82,
+           position = position_dodge(width = 0.82),
+           show.legend = T)+
+  geom_text(data = araneae_gr_occ_tax_f_l,
+            aes(x = Family ,y= number, label=number,group=Variable),
+            position=position_dodge(width = 0.87),
+            vjust=-0.25,
+            size=5)+
+#  scale_y_continuous(breaks = seq(0,280,20),
+#                     limits = c(0,285),
+#                     expand = c(0.01,0.4))+
+#  scale_x_discrete(expand = c(0.01,0.4))+
+#  scale_fill_manual(label=c("Caves","All species","Species endemic to Greece","Troglobiont species"),
+#                    values = c("coral1","lightgoldenrod2","lightpink1","lightblue1"),
+#                    name="")+
+  labs(x="Family", y= "Count")+
+  theme_bw()+
+  theme(panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank(),
+        legend.text = element_text(size = 18),
+        axis.text.y=element_text(margin = margin(t = 0, r = 0, b = 0, l = 10,unit = "pt"),size = 18),
+        axis.text.x = element_text(angle = 45, hjust = 1,size = 18),
+        axis.title = element_text(size=22),
+        panel.border = element_blank(),
+        axis.line.x = element_line(colour = 'black', linewidth = 0.3),
+        axis.line.y = element_line(colour = 'black', linewidth = 0.3),
+        legend.position = c(0.13,0.87),
+        legend.key.size = unit(1, "cm"))
+
+ggsave("species_family_plot.png",
+       plot = species_family_plot,
+       device = "png",
+       width = 50,
+       height = 36,
+       units = "cm",
+       dpi = 100,
+       path = "../figures/")
+
+
